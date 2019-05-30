4827e5c2d76b25f06bbc77912992d3315b54351d
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-05-29 17:58:07 -0700

    China script done

--- ChinaCargo/convertToPost.py
+++ ChinaCargo/convertToPost.py
@@ -72,16 +72,68 @@ class baseInfo:
     "workOrderNumber": None
     }
 
+def ChinaEvent(event):
+    if(event.find("Delivered") != -1):
+        return ("DLV", "Delivered")
+    elif(event.find("Notified") != -1):
+        return ("NFD", "Consignee/Agent notified of arrival")
+    elif(event.find("Received") != -1):
+        return ("RCF", "Received from Flight")
+    elif(event.find("Arrived") != -1):
+        return ('ARR', "Arrived")
+    elif(event.find("Departed") != -1):
+        return ('DEP', "Departed on Flight")
+    elif(event.find("Booked") != -1):
+        return ('BKD', "Booked")
+    elif(event.find("Air Waybill Delivered") != -1):
+        return ('DDC', "Arrival documents delivered to consignee/agent")
+    return (None, None)
+
 def ChinaPost(step):
+    with open(step) as json_file:  
+        data = json.load(json_file)
+    postJson = copy.deepcopy(baseInfo.shipmentEventBase)
+    postJson["resolvedEventSource"] = "China RPA"
+    postJson["reportSource"] = "AirEvent"
+    postJson["workOrderNumber"] = data.get("Work Order")
+    postJson["shipmentReferenceNumber"] = data.get("Reference Number")
+    postJson["unitId"] = data.get("Waybill")
+    postJson["location"] = data.get("Event Airport")
+    postJson["vessel"] = data.get("Flight")
+    postJson["eventCode"], postJson["eventName"] = ChinaEvent(data.get("Status"))
+    postJson["carrierName"] = data.get("Air Carrier")
+    if(postJson["eventCode"] == None):
+        return
+    dt = datetime.datetime.strptime(data.get("Event Time").split("(")[0], "%d%b %H:%M")
+    dt = dt.replace(year=datetime.datetime.now().year)
+    if(dt.date() > datetime.datetime.today().date()):
+        dt = dt.replace(year = datetime.datetime.year-1)
+    postJson["eventTime"] = dt.strftime('%m-%d-%Y %H:%M:%S')
+    print(json.dumps(postJson))
+    #postJson["weight"] = data.get("Weight")
+    #postJson["quantity"] = data.get("Pieces")
+    headers = {'content-type':'application/json'}
+    r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
+    print(json.dumps(postJson))
+    print(r)
     return
 
-def main(containerList, cwd):
+def testMain(container): #test main
+    fileList = glob.glob(os.getcwd() + "\\ContainerInformation\\"+container+"Step*.json", recursive = True) #get all the json steps
+    #print(os.getcwd() + "\\ContainerInformation\\"+container+"Step*.json")
+    if (not fileList):
+        return
+    fileList = [f for f in fileList if container in f] #set of steps for this number
+    fileList.sort(key=os.path.getmtime) #order steps correctly (by file edit time)
+    for step in fileList:
+        ChinaPost(step)
+
+def main(containerList, cwd): #real main
     path=""
     for x in cwd.split("\\"):
         path+=x+"\\\\" #just to add escape sequences for the glob method to work fine
     for container in containerList:
         fileList = glob.glob(path + "ContainerInformation\\"+container+"Step*.json", recursive = True) #get all the json steps
-        
         if (not fileList):
             continue
         fileList = [f for f in fileList if container in f] #set of steps for this number
@@ -90,4 +142,5 @@ def main(containerList, cwd):
             ChinaPost(step)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    testMain(sys.argv[1])
+    #main(sys.argv[1], sys.argv[2])
