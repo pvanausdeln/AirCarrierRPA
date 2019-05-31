74e25b0c1f66933f93d75cb26eabaa0a4f2ac6c0
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-05-31 14:24:53 -0700

    Qantas script done

--- QantasCargo/convertToPost.py
+++ QantasCargo/convertToPost.py
@@ -72,8 +72,57 @@ class baseInfo:
     "workOrderNumber": None
     }
 
+def QantasEvent(event):
+    if(event.find("Booked") != -1):
+        return ("BKG", "Shipment Booked")
+    elif(event.find("Accepted") != -1):
+        return ('RCS', "Received from Shipper")
+    elif(event.find("Manifested") != -1):
+        return ("MAN", "Manifested")
+    elif(event.find("Departed") != -1):
+        return ('DEP', "Departed")
+    elif(event.find("Checked In") != -1):
+        return ("CIN", "Checked In")
+    elif(event.find("Delivered") != -1):
+        return ('DLV', "Delivered")
+    elif(event.find("Plan Update") != -1):
+        return ('BKU', "Booking Updated")
+    return (None, None)
+
 def QantasPost(step):
+    with open(step) as json_file:  
+        data = json.load(json_file)
+    postJson = copy.deepcopy(baseInfo.shipmentEventBase)
+    postJson["resolvedEventSource"] = "Qantas RPA"
+    postJson["reportSource"] = "AirEvent"
+    postJson["workOrderNumber"] = data.get("Work Order")
+    postJson["shipmentReferenceNumber"] = data.get("Reference Number")
+    postJson["unitId"] = data.get("Waybill")
+    postJson["location"] = data.get("Station")
+    postJson["eventCode"], postJson["eventName"] = QantasEvent(data.get("Event"))
+    postJson["carrierName"] = data.get("Air Carrier")
+    postJson["vessel"] = data.get("Flight/Date")
+    if(postJson["eventCode"] == None):
+        return
+    dt = datetime.datetime.strptime(data.get("Date"), "%d-%b-%Y %H:%M:%S")
+    postJson["eventTime"] = dt.strftime('%m-%d-%Y %H:%M:%S')
+    print(json.dumps(postJson))
+    #postJson["weight"] = data.get("Weight")
+    #postJson["quantity"] = data.get("Piece(s)")
+    headers = {'content-type':'application/json'}
+    r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
+    print(json.dumps(postJson))
+    print(r)
     return
+
+def testMain(container): #test main
+    fileList = glob.glob(os.getcwd() + "\\ContainerInformation\\"+container+"Step*.json", recursive = True) #get all the json steps
+    if (not fileList):
+        return
+    fileList = [f for f in fileList if container in f] #set of steps for this number
+    fileList.sort(key=os.path.getmtime) #order steps correctly (by file edit time)
+    for step in fileList:
+        QantasPost(step)
 
 def main(containerList, cwd):
     path=""
@@ -81,7 +130,6 @@ def main(containerList, cwd):
         path+=x+"\\\\" #just to add escape sequences for the glob method to work fine
     for container in containerList:
         fileList = glob.glob(path + "ContainerInformation\\"+container+"Step*.json", recursive = True) #get all the json steps
-        
         if (not fileList):
             continue
         fileList = [f for f in fileList if container in f] #set of steps for this number
@@ -90,4 +138,5 @@ def main(containerList, cwd):
             QantasPost(step)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    testMain(sys.argv[1])
+    #main(sys.argv[1], sys.argv[2])
