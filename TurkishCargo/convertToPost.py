493ba27b2971e5d661eb568e1a3a148479cb3f61
pvanausdeln <45799510+pvanausdeln@users.noreply.github.com>
2019-05-31 17:17:30 -0700

    turkish script done

--- TurkishCargo/convertToPost.py
+++ TurkishCargo/convertToPost.py
@@ -72,8 +72,59 @@ class baseInfo:
     "workOrderNumber": None
     }
 
+def TurkishEvent(event):
+    if(event.find("RCS") != -1):
+        return ('RCS', "Received from Shipper")
+    elif(event.find("RCF") != -1):
+        return ('RCF', "Received from Shipper")
+    elif(event.find("DEP") != -1):
+        return ('DEP', "Departed")
+    elif(event.find("DLV") != -1):
+        return ('DLV', "Delivered")
+    elif(event.find("BKD") != -1):
+        return ('BKG', "Shipment Booked")
+    elif(event.find("MAN") != -1):
+        return ("MAN", "Manifested")
+    elif(event.find("NFD") != -1):
+        return ("NFD", "Consignee/Agent notified of arrival")
+    return (None, None)
+
 def TurkishPost(step):
+    with open(step) as json_file:  
+        data = json.load(json_file)
+    postJson = copy.deepcopy(baseInfo.shipmentEventBase)
+    postJson["resolvedEventSource"] = "Turkish RPA"
+    postJson["reportSource"] = "AirEvent"
+    postJson["workOrderNumber"] = data.get("Work Order")
+    postJson["shipmentReferenceNumber"] = data.get("Reference Number")
+    postJson["unitId"] = data.get("Waybill")
+    postJson["location"] = data.get("Station Code")
+    postJson["eventCode"], postJson["eventName"] = TurkishEvent(data.get("Tracking Process"))
+    postJson["carrierName"] = data.get("Air Carrier")
+    postJson["vessel"] = data.get("Flight").split(",")[0]
+    #try:
+    #    postJson["Weight"] = data.get("Weight")
+    #    postJson["Pieces"] = data.get("Pieces")
+    #except:
+    #    pass
+    if(postJson["eventCode"] == None):
+        return
+    dt = datetime.datetime.strptime(data.get("Actual Time"), "%d %b %Y %H:%M")
+    postJson["eventTime"] = dt.strftime('%m-%d-%Y %H:%M:%S')
+    headers = {'content-type':'application/json'}
+    r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
+    print(json.dumps(postJson))
+    print(r)
     return
+
+def testMain(container): #test main
+    fileList = glob.glob(os.getcwd() + "\\ContainerInformation\\"+container+"Step*.json", recursive = True) #get all the json steps
+    if (not fileList):
+        return
+    fileList = [f for f in fileList if container in f] #set of steps for this number
+    fileList.sort(key=os.path.getmtime) #order steps correctly (by file edit time)
+    for step in fileList:
+        TurkishPost(step)
 
 def main(containerList, cwd):
     path=""
@@ -90,4 +141,5 @@ def main(containerList, cwd):
             TurkishPost(step)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    testMain(sys.argv[1])
+    #main(sys.argv[1], sys.argv[2])
