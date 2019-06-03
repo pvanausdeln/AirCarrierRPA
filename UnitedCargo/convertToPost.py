369740baa878952a2cb2b9ed73337c3b79748b5c
sow-blu <sowrab.iyengar@blumeglobal.com>
2019-06-03 13:18:53 +0530

    Script for united cargo

--- UnitedCargo/convertToPost.py
+++ UnitedCargo/convertToPost.py
@@ -72,7 +72,59 @@ class baseInfo:
     "workOrderNumber": None
     }
 
+def UnitedPostEvent(event):
+    if(event.find("Received from flight") != -1):
+        return ("RCF", "Received from flight")
+    elif(event.find("Delivered") != -1):
+        return ("DLV", "Delivered")
+    elif(event.find("Documents delivered") != -1):
+        return ("DDC", "Documents delivered")
+    elif(event.find("Arrived") != -1):
+        return ('ARR', "Arrived")
+    elif(event.find("Departed") != -1):
+        return ('DEP', "Departed")
+    elif(event.find("Manifested") != -1):
+        return ('MAN', "Manifested")
+    elif(event.find("Received from other airline") != -1):
+        return ('RCA', "Received from other airline")
+    elif(event.find("Transferred") != -1):
+        return ('TAA', "Transferred")
+    elif(event.find("Booking Confirmed") != -1):
+        return ('BKG', "Booking Confirmed")
+    elif(event.find("Received from shipper") != -1):
+        return ('RCS', "Received from shipper")
+    elif(event.find("Documents received") != -1):
+        return ('DDA', "Documents received")
+    return (None, None)
+
 def UnitedPost(step):
+    with open(step) as json_file:  
+        data = json.load(json_file)
+    postJson = copy.deepcopy(baseInfo.shipmentEventBase)
+    postJson["resolvedEventSource"] = "United Cargo RPA"
+    postJson["reportSource"] = "AirEvent"
+    postJson["workOrderNumber"] = data.get("Work Order")
+    postJson["shipmentReferenceNumber"] = data.get("Reference Number")
+    postJson["unitId"] = data.get("Waybill")
+    postJson["location"] = data.get("Station")
+    #postJson["vessel"] = data.get("Flight Details")
+    postJson["eventCode"], postJson["eventName"] = UnitedPostEvent(data.get("Status"))
+    postJson["carrierName"] = data.get("Air Carrier")
+    postJson["eventTime"]=data.get("Date/Time")
+    if(postJson["eventCode"] == None):
+        return
+    dt = datetime.datetime.strptime(data.get("Event Time").split("(")[0], "%d%b %H:%M")
+    dt = dt.replace(year=datetime.datetime.now().year)
+    if(dt.date() > datetime.datetime.today().date()):
+        dt = dt.replace(year = datetime.datetime.year-1)
+    postJson["eventTime"] = dt.strftime('%m-%d-%Y %H:%M:%S')
+    print(json.dumps(postJson))
+    #postJson["weight"] = data.get("Weight")
+    #postJson["quantity"] = data.get("Pieces")
+    headers = {'content-type':'application/json'}
+    r = requests.post(baseInfo.postURL, data = json.dumps(postJson), headers = headers, verify = False)
+    print(json.dumps(postJson))
+    print(r)
     return
 
 def main(containerList, cwd):
